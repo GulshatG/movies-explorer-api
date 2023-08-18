7b8c7ce1ed7fae1152fee3c556374a17891161a9
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-08-19 02:49:12 +0400

    fix cookies

--- cotrollers/userController.js
+++ cotrollers/userController.js
@@ -73,6 +73,7 @@ module.exports.login = (req, res, next) => {
         res.cookie('token', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: 'none',
         });
         const userObj = user.toObject();
         delete userObj.password;
